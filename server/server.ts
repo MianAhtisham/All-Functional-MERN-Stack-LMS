c633e4bfdc1e0c05e17c6d18bb2bdcdc406a82a9
MianAhtisham <meharyounes067@gmail.com>
2025-09-25 16:26:25 +0500

    first commit

---
+++ server/server.ts
@@ -0,0 +1,8 @@
+import { app } from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is connected with port ${process.env.PORT}`);
+});
